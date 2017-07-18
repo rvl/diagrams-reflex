8e624dcac653929db2440bc1e930b786765d0bc5
Rodney Lorrimar <dev@rodney.id.au>
2017-07-18 22:35:37 +0100

    build hacks

--- ghcjs.nix
+++ ghcjs.nix
@@ -1,1 +1,7 @@
-{ reflex-platform,  ... }: reflex-platform.ghcjs
+{ reflex-platform,  pkgs ? import <nixpkgs> {}, ... }: reflex-platform.ghcjs.override {
+  overrides = self: super: {
+    reflex-dom-contrib = self.callPackage ./reflex-dom-contrib.nix {
+      inherit (pkgs) fetchFromGitHub;
+    };
+  };
+}
